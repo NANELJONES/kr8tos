0419bd1d8ef885eb2ade97c9f1580499abcd323d
theshimmer <60219627+theshimmer1@users.noreply.github.com>
2023-02-06 21:10:33 +0000

    boy

--- components/Header.js
+++ components/Header.js
@@ -29,7 +29,7 @@ const Header = () => {
          <div className='font-Montserrat text-3xl flex flex-col items-center gap-6 overflow-x-hidden'>
             <video autoPlay={true} muted={true} className="z-10 " >
                   <source src={"/assets/just logo animation.webm"} ></source>
-                  <source src={"/assets/3D LOGIN.mov"} ></source>
+                  <source src={"/3D LOGIN.mov"} ></source>
              
              
               </video>
