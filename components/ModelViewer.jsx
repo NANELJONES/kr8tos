1d2586aea33bd1a45b307bbe84eef30d5867d6ce
theshimmer <60219627+theshimmer1@users.noreply.github.com>
2023-02-07 12:08:03 +0000

    no post processing

--- components/ModelViewer.jsx
+++ components/ModelViewer.jsx
@@ -17,18 +17,18 @@ const ModelViewer = ({ modelPath, scale = 40, position = [0, 0, 0] }) => {
     <spotLight position={[10, 10, 10]} angle={2} intensity={6}   penumbra={1} />
     <pointLight position={[10, 10, -10]} intensity={2} color={"#0000FF"} />
     <pointLight position={[20, 10, 0]} intensity={1}  />
-    {/* <Suspense fallback={null}> */}
+    <Suspense fallback={null}> 
     <Model modelPath={modelPath} scale={scale} position={position} />
     <OrbitControls />
   
-    {/* </Suspense> */}
-    <EffectComposer>
+   </Suspense>
+    {/* <EffectComposer>
     <DepthOfField focusDistance={0} focalLength={0.09} bokehScale={0.02} height={480} />
         <Bloom luminanceThreshold={0.2} luminanceSmoothing={1} height={300} />
        
         <Vignette eskil={false} offset={0.4} darkness={0.6} />
 
-    </EffectComposer>
+    </EffectComposer> */}
   
 
   </Canvas>
