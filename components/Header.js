1239abe989ad0ccd5db15f11a721e4f5f55f21cc
theshimmer <60219627+theshimmer1@users.noreply.github.com>
2023-02-07 11:32:03 +0000

    remov slide

--- components/Header.js
+++ components/Header.js
@@ -29,7 +29,7 @@ const Header = () => {
          <div className='font-Montserrat text-3xl flex flex-col items-center gap-6 overflow-x-hidden'>
             <video autoPlay={true} muted={true} className="z-10 " >
                   <source src={"/assets/just logo animation.webm"} ></source>
-                  <source src={"/assets/3D LOGIN.mov"} ></source>
+                  <source src={"/assets/just logo animation real.mov"} ></source>
              
              
               </video>
@@ -43,7 +43,7 @@ const Header = () => {
                
          
           </div>
-          <video autoPlay={true} loop={true} muted={true} src={"/assets/render waves.webm"} className=" absolute top-[25vw]  lg:top-[20vw] xl:top-[12vw] w-auto  "/>
+          <video autoPlay={true} loop={true} muted={true} src={"/assets/render waves.mp4"} className=" absolute top-[25vw]  lg:top-[20vw] xl:top-[12vw] w-auto  "/>
 
          
 
