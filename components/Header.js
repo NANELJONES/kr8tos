ca79541bb71c8d9ae7a292d804e7ac6324106591
theshimmer <60219627+theshimmer1@users.noreply.github.com>
2023-02-09 12:36:37 +0000

    savor

--- components/Header.js
+++ components/Header.js
@@ -26,10 +26,10 @@ const Header = () => {
     <div className='relative '>
 
         
-         <div className='font-Montserrat text-3xl flex flex-col items-center gap-6 overflow-x-hidden'>
-            <video autoPlay={true} loop={true} muted={true} className="z-10  h-[60vw] object-cover  sm:aspect-video md:h-[auto]  md:aspect-auto " >
+         <div className='font-Montserrat text-3xl  flex flex-col items-center gap-6 overflow-x-hidden'>
+            <video autoPlay={true} playsInline loop={true} muted={true}  className="  z-10  h-[60vw] object-cover  sm:aspect-video md:h-[auto]  md:aspect-auto " >
                    <source src={"/assets/animation.mp4" } type="video/mp4" ></source>
-                   
+                      <source src={"/assets/animation ios.mov" } type="video/mp4" ></source>
                      <source src={"/assets/animation ios.mov" } type="video/mov" ></source>
                   {/* <source src={"/assets/just logo animation.webm"} ></source>
                   <source src={"/assets/safari_logo_animation.mp4"}  type="video/mp4" ></source>
