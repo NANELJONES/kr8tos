9ca01dba44ea3418d6ed8e753caa5a582b1977e9
theshimmer <60219627+theshimmer1@users.noreply.github.com>
2023-10-13 13:19:59 +0000

    animo

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,61 +1,45 @@
-import React from 'react'
-import Image from 'next/image';
-
-
+import React from "react";
+import Image from "next/image";
 
 const Projects = () => {
-
-    const new_array =()=>{
-        var arr= []
-        for(var i =1; i< 9; i++){
-            arr.push(i)
-        
-        }
-        return arr
-    
+  const new_array = () => {
+    var arr = [];
+    for (var i = 1; i < 9; i++) {
+      arr.push(i);
     }
+    return arr;
+  };
 
-    const sci_image = [
-      "/assets/sample_project/1.jpg",
-      "/assets/sample_project/2.jpg",
-      "/assets/sample_project/3.jpg",
-      "/assets/sample_project/4.jpg",
-      "/assets/sample_project/5.jpg",
-      "/assets/sample_project/6.jpg",
-      "/assets/sample_project/7.jpg",
-      "/assets/sample_project/8.png",
-      
-    ]
-    
-
+  const sci_image = [
+    "/assets/sample_project/1.jpg",
+    "/assets/sample_project/2.jpg",
+    "/assets/sample_project/3.jpg",
+    "/assets/sample_project/4.jpg",
+    "/assets/sample_project/5.jpg",
+    "/assets/sample_project/6.jpg",
+    "/assets/sample_project/7.jpg",
+    "/assets/sample_project/8.png",
+  ];
 
   return (
-
-
-    
-    <div className='flex    font-Montserrat flex-col items-center  text-[10vw] text-[white] '>
-
-    <h2 className='font-bold text-white text-[5vw] md:text-[2.5vw] md:w-[20%] w-[50%] text-center 2xl:text-[45px]'>PROJECTS & CLIENTS </h2>
-  <div className=' border-t-[1vw] border-[orange] pt-[1vw] grid grid-cols-4 gap-4 mt-[3vw] duration-700'>
-            {sci_image.map((each) =>{
-
-            return(
-            <div key={each} className="hover:scale-110 duration-300 relative w-[20vw] h-[25vw] max-w-[300px] max-h-[350px]">
-                    
-                 
-
+    <div className="flex    font-Montserrat flex-col items-center  text-[10vw] text-[white] ">
+      <h2 className="font-bold text-white text-[5vw] md:text-[2.5vw] md:w-[20%] w-[50%] text-center 2xl:text-[45px]">
+        PROJECTS & CLIENTS{" "}
+      </h2>
+      <div className=" border-t-[1vw] border-[orange] pt-[1vw] grid grid-cols-4 gap-4 mt-[3vw] duration-700">
+        {sci_image.map((each) => {
+          return (
+            <div
+              key={each}
+              className="hover:scale-110 duration-300 relative w-[20vw] h-[25vw] max-w-[300px] max-h-[350px]"
+            >
+              <Image fill src={each} className="object-cover"></Image>
             </div>
-            )
-            })}
-
-  </div>
-
-
-
-  
-
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
--- pages/portfolio/[slug].jsx
+++ pages/portfolio/[slug].jsx
@@ -1,34 +1,34 @@
-import React, { lazy } from 'react'
-import { useRouter } from 'next/router';
-import { storage } from '../../firebase/firebase';
-import {ref,listAll,getDownloadURL} from "firebase/storage"
-import { useState,useEffect } from 'react';
+import React, { lazy } from "react";
+import { useRouter } from "next/router";
+import { storage } from "../../firebase/firebase";
+import { ref, listAll, getDownloadURL } from "firebase/storage";
+import { useState, useEffect } from "react";
 import Link from "next/link";
-import Image from 'next/image';
-import { async } from '@firebase/util';
-import Modal from '../../components/Modal';
-import {v4 as uuidv4} from "uuid";
-import { useRef } from 'react';
-
-
+import Image from "next/image";
+import { async } from "@firebase/util";
+import Modal from "../../components/Modal";
+import { v4 as uuidv4 } from "uuid";
+import { useRef } from "react";
 
 const Portfolio = () => {
-    const vid_ref = useRef("")
+  const vid_ref = useRef("");
 
-    const [all_images, setall_images] = useState([])
-   
-    const [desc, set_desc] = useState("We design and brand your ideas, company and vision by creating a cohesive visual language to better visualize your products, concepts and ideas. We create these designs to appeal what you want to sell to your target audience. Our team of talented Digital , Traditional and Artificial Intelligence Artists create all forms of artworks and illustrations which best suits your interest.")
-    const routes =  useRouter()
-    const new_path =  routes.query.slug
+  const [all_images, setall_images] = useState([]);
 
+  const [desc, set_desc] = useState(
+    "We design and brand your ideas, company and vision by creating a cohesive visual language to better visualize your products, concepts and ideas. We create these designs to appeal what you want to sell to your target audience. Our team of talented Digital , Traditional and Artificial Intelligence Artists create all forms of artworks and illustrations which best suits your interest."
+  );
+  const routes = useRouter();
+  const new_path = routes.query.slug;
 
-    const [modal, setmodal] = useState({show_modal:false, url:""})
-  
+  const [modal, setmodal] = useState({ show_modal: false, url: "" });
 
-    const  isImgLink=(url)=> {
-      if(typeof url !== 'string') return false;
-      return(url.match(/^http[^\?]*.(jpg|jpeg|gif|png|tiff|bmp)(\?(.*))?$/gmi) != null);
-  }
+  const isImgLink = (url) => {
+    if (typeof url !== "string") return false;
+    return (
+      url.match(/^http[^\?]*.(jpg|jpeg|gif|png|tiff|bmp)(\?(.*))?$/gim) != null
+    );
+  };
 
   // const [scroll_position, setscroll_position] = useState()
 
@@ -38,149 +38,169 @@ const Portfolio = () => {
 
   // }
 
-
-
   // useEffect(() => {
   //   handle_scroll
   //   window.addEventListener("scroll",handle_scroll );
-  
+
   //   return () => {
   //     window.removeEventListener("scroll",handle_scroll)
   //   }
   // }, [])
-  
- 
-   useEffect(()=>{
-    setall_images([])
 
-    const new_images =  ref(storage, new_path);
+  useEffect(() => {
+    setall_images([]);
+
+    const new_images = ref(storage, new_path);
     listAll(new_images)
       .then((res) => {
-     
-        res.items.forEach((itemRef) => {      
-           
-          
-           getDownloadURL(ref(storage, itemRef.fullPath)).then((url)=>{
-           
-          setall_images(all_images  => [...all_images, url])
-           })
+        res.items.forEach((itemRef) => {
+          getDownloadURL(ref(storage, itemRef.fullPath)).then((url) => {
+            setall_images((all_images) => [...all_images, url]);
+          });
         });
-      }).catch((error) => {
-       console.log("An Error has occurred")
+      })
+      .catch((error) => {
+        console.log("An Error has occurred");
       });
+  }, [new_path]);
 
-    },[new_path])
-
-  const var_btns=[
-    {title:"Art & Design",
-      slug:"Design",
-      desc:"We design and brand your ideas, company and vision by creating a cohesive visual language to better visualize your products, concepts and ideas. We create these designs to appeal what you want to sell to your target audience. Our team of talented Digital , Traditional and Artificial Intelligence Artists  create all forms of artworks and illustrations which best suits your interest."
+  const var_btns = [
+    {
+      title: "Art & Design",
+      slug: "Design",
+      desc: "We design and brand your ideas, company and vision by creating a cohesive visual language to better visualize your products, concepts and ideas. We create these designs to appeal what you want to sell to your target audience. Our team of talented Digital , Traditional and Artificial Intelligence Artists  create all forms of artworks and illustrations which best suits your interest.",
     },
 
-      {title:"Software",
-      slug:"Software Development",
-      desc:"We develop Websites, Apps, Databases, Backend Systems, Artificial Intelligent(AI) Systems, Expert Systems, UX/UI etc."
+    {
+      title: "Software",
+      slug: "Software Development",
+      desc: "We develop Websites, Apps, Databases, Backend Systems, Artificial Intelligent(AI) Systems, Expert Systems, UX/UI etc.",
     },
-    
-    {title:"Animation",
-    slug:"Animation",
-    desc:"We are equipped with animators to produce High End 2d, 3d and Motion Design and Motion Graphics Animations for every scope where the use of animation as a whole is applicable."
-  },
 
-  {title:"3D",
-  slug:"3D",
-  desc:"Our Creative Studio has a dedicated team of talented 3D Artists and Designers to design 3D concepts and Animations. Some of our 3D services include, 3D Modeling, Architectural Visualizations, Hard Surface Modeling, Product Design, 3D Fashion, Game Asset modeling, etc."
-},
+    {
+      title: "Animation",
+      slug: "Animation",
+      desc: "We are equipped with animators to produce High End 2d, 3d and Motion Design and Motion Graphics Animations for every scope where the use of animation as a whole is applicable.",
+    },
 
-{title:"Media Production",
-slug:"Media_Production",
-desc:"As a company, we provide multimedia Production services like Photography, Videography, Visual Effects, Voice Overs, Music Production, etc."
-},
-    
-    ]
+    {
+      title: "3D",
+      slug: "3D",
+      desc: "Our Creative Studio has a dedicated team of talented 3D Artists and Designers to design 3D concepts and Animations. Some of our 3D services include, 3D Modeling, Architectural Visualizations, Hard Surface Modeling, Product Design, 3D Fashion, Game Asset modeling, etc.",
+    },
 
-    const closeModal = ()=>{
-      setmodal( {...modal, show_modal:!modal.show_modal} )
-    }
+    {
+      title: "Media Production",
+      slug: "Media_Production",
+      desc: "As a company, we provide multimedia Production services like Photography, Videography, Visual Effects, Voice Overs, Music Production, etc.",
+    },
+  ];
 
+  const closeModal = () => {
+    setmodal({ ...modal, show_modal: !modal.show_modal });
+  };
 
-
-     function display( a){
-      console.log(new_path)
-      console.log(a)
-    }
+  function display(a) {
+    console.log(new_path);
+    console.log(a);
+  }
 
   return (
-    
-    <div className='h-auto min-h-[110vh] relative '
-    style={{
-      backgroundImage: `url(/bg.jpg)`,
-      backgroundSize: 'cover',
-      backgroundRepeat: 'repeat',
-      backgroundPosition: 'center',
-      
-    }}
-    > 
+    <div
+      className="h-auto min-h-[110vh] relative "
+      style={{
+        backgroundImage: `url(/bg.jpg)`,
+        backgroundSize: "cover",
+        backgroundRepeat: "repeat",
+        backgroundPosition: "center",
+      }}
+    >
+      {modal.show_modal ? (
+        <Modal
+          url={modal.url}
+          onClick={() => {
+            setmodal({ ...modal, show_modal: !modal.show_modal });
+          }}
+        />
+      ) : null}
 
-    {modal.show_modal ? <Modal url={modal.url}   onClick={()=>{setmodal( {...modal, show_modal:!modal.show_modal} )}} /> : null} 
-   
+      <div className="font-Montserrat pt-[2vw]  mx-auto text-white flex items-center gap-[3vw] flex-wrap justify-evenly w-full md:w-[80%] lg:w-[60%]">
+        {var_btns.map((each_btn) => {
+          return (
+            <Link href={`/portfolio/${each_btn.slug}`}>
+              <button
+                key={var_btns.indexOf(each_btn)}
+                onClick={() => {
+                  set_desc(each_btn.desc), display(each_btn.slug);
+                }}
+                className={` ${
+                  each_btn.slug === new_path ? "bg-[orange]" : "bg-k_blue"
+                }   mx-auto   duration-300 md:hover:text-[1.1vw] md:hover:font-thin text-center text-[2vw] h-[7vw] w-[20vw] md:max-w-[12vw] lg:text-[0.8vw] lg:max-w-[12vw]   md:h-[4vw] lg:h-[3vw]  text-center font-Monserrat   font-light   md:text-[1.1vw] font-light`}
+              >
+                {each_btn.title}
+              </button>
+            </Link>
+          );
+        })}
+      </div>
 
-            <div className='font-Montserrat pt-[2vw]  mx-auto text-white flex items-center gap-[3vw] flex-wrap justify-evenly w-full md:w-[80%] lg:w-[60%]' >
-                  {var_btns.map((each_btn)=>{
-                              return(
+      <p className="mt-[5vw] md:mt-[2.5vw] font-Montserrat text-white font-light text-center text-[3vw] md:text-[1.5vw] md:w-[60%] lg:text-[1vw] lg:w-[50%] lg:leading-[2.4vw]  duration-300  w-[80%] mx-auto leading-[4.5vw] lg:mt-[1.4vw]">
+        {desc}
+      </p>
+      <div className="w-full flex items-center justify-evenly">
+        <button className=" text-[2vw] font-Montserrat font-regular text-white text-center  bg-k_red  px-[4vw] py-[2vw] md:text-[0.7vw] md:px-[3em] md:py-[1em] hover:bg-[orange] hover:text-white hover:px-[3.2em] hover:py-[1.2em] hover:text-1.5xl duration-500 hover:font-bold ">
+          {" "}
+          HIRE US {">>"}{" "}
+        </button>
+      </div>
 
-                              <Link   
-                              href={`/portfolio/${each_btn.slug}`}><button key={var_btns.indexOf(each_btn)   } 
-                              onClick={()=>{set_desc(each_btn.desc), display(each_btn.slug) }}  
-                              className={` ${each_btn.slug === new_path ? "bg-[orange]": "bg-k_blue"}   mx-auto   duration-300 md:hover:text-[1.1vw] md:hover:font-thin text-center text-[2vw] h-[7vw] w-[20vw] md:max-w-[12vw] lg:text-[0.8vw] lg:max-w-[12vw]   md:h-[4vw] lg:h-[3vw]  text-center font-Monserrat   font-light   md:text-[1.1vw] font-light`} >{each_btn.title}</button></Link>
-                                  
-                              ) 
-                            })}
-                  </div>
-
-                  <p className='mt-[5vw] md:mt-[2.5vw] font-Montserrat text-white font-light text-center text-[3vw] md:text-[1.5vw] md:w-[60%] lg:text-[1vw] lg:w-[50%] lg:leading-[2.4vw]  duration-300  w-[80%] mx-auto leading-[4.5vw] lg:mt-[1.4vw]'>{desc}</p>
-                  <div className='w-full flex items-center justify-evenly'>
-                  <button className=' text-[2vw] font-Montserrat font-regular text-white text-center  bg-k_red  px-[4vw] py-[2vw] md:text-[0.7vw] md:px-[3em] md:py-[1em] hover:bg-[orange] hover:text-white hover:px-[3.2em] hover:py-[1.2em] hover:text-1.5xl duration-500 hover:font-bold '> HIRE US {">>"} </button>
-                  </div>
-             
-         
-          <div className=' mt-[3vw]  flex items-baseline flex-wrap gap-[1vw] h-auto  border-white md:w-auto mx-auto'> 
-
-              {all_images && all_images.map((each_image)=>{
-                  return(
-                    <div  onClick={()=>
-                      {setmodal( {...modal, show_modal:!modal.show_modal, url:each_image} )
-                      
-                    }
-                     
-                
-                    
-                    }  
-                       key={uuidv4()} 
-                   
-                    className="relative text-white w-[30%] h-[40vw] cursor-pointer mx-auto  hover:scale-125 hover:z-[10] duration-300  md:w-[30%] md:h-[40vw]  object-cover  max-h-[300px] max-w-[200px] ">
-                      
-                        {isImgLink(each_image) ? <Image placeholder='blur' blurDataURL='/loading.jpg'    src={each_image} className="object-cover " fill   alt="/blur" /> : <video onClick={(e)=>{ vid_ref.pause}} ref={vid_ref} muted autoPlay={true} className="w-full  h-full object-cover"  controls src={each_image}/>}
-                      
-                    </div>
-
-                    
-                    
-                  )
-                }) }   
-                
-
-          </div>
-
-         
-
+      <div className=" mt-[3vw]  flex items-baseline flex-wrap gap-[1vw] h-auto  border-white md:w-auto mx-auto">
+        {all_images &&
+          all_images.map((each_image) => {
+            return (
+              <div
+                onClick={() => {
+                  setmodal({
+                    ...modal,
+                    show_modal: !modal.show_modal,
+                    url: each_image,
+                  });
+                }}
+                key={uuidv4()}
+                className="relative text-white w-[30%] h-[40vw] cursor-pointer mx-auto  hover:scale-125 hover:z-[10] duration-300  md:w-[30%] md:h-[40vw]  object-cover  max-h-[300px] max-w-[200px] "
+              >
+                {isImgLink(each_image) ? (
+                  <Image
+                    placeholder="blur"
+                    blurDataURL="/loading.jpg"
+                    lazy
+                    src={each_image}
+                    className="object-cover "
+                    fill
+                    alt="/blur"
+                  />
+                ) : (
+                  <video
+                    onClick={(e) => {
+                      vid_ref.pause;
+                    }}
+                    ref={vid_ref}
+                    muted
+                    autoPlay={true}
+                    className="w-full  h-full object-cover"
+                    controls
+                    src={each_image}
+                  />
+                )}
+              </div>
+            );
+          })}
+      </div>
     </div>
-  )
-}
-
+  );
+};
 
 export default Portfolio;
-
 
 // export async function getStaticProps(){
 //   const routes =  useRouter()
@@ -189,11 +209,11 @@ export default Portfolio;
 //     setall_images([])
 //     listAll(new_images)
 //       .then((res) => {
-     
-//         res.items.forEach((itemRef) => {        
-          
+
+//         res.items.forEach((itemRef) => {
+
 //            getDownloadURL(ref(storage, itemRef.fullPath)).then((url)=>{
-           
+
 //             total_images.push(url)
 //           // setall_images(all_images  => [...all_images, url])
 
@@ -202,7 +222,6 @@ export default Portfolio;
 //       }).catch((error) => {
 //        console.log("An Error has occurred")
 //       });
-      
 
 //       return{
 //         props:[total_images]
@@ -210,18 +229,17 @@ export default Portfolio;
 // }
 
 // export async function getStaticPaths() {
-  
-  
+
 //   const new_images =  ref(storage, "Design");
 //   const total_paths = []
-    
+
 //     listAll(new_images)
 //       .then((res) => {
-     
-//         res.items.forEach((itemRef) => {        
+
+//         res.items.forEach((itemRef) => {
 
 //           total_paths.push({params:{id:itemRef.name}})
-         
+
 //         });
 //       }).catch((error) => {
 //        console.log("An Error has occurred")
@@ -231,5 +249,4 @@ export default Portfolio;
 //         paths:total_paths, fallback:false
 //       }
 
- 
 // }
